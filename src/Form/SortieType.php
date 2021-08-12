2bbacd3b919a0faf549074d7341f0d5feff6b02d
morad <morad.el.mansouri@outlook.fr>
2021-08-12 11:44:29 +0200

    formulaire créer sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label'=>'Nom de la sortie',
+            ])
+            ->add('dateHeureDebut', DateType::class, [
+                'label' => 'Date et heure de la sortie :',
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('duree', IntegerType::class, [
+                'label'=>'Durée',
+            ])
+            ->add('dateLimiteinscription', DateType::class, [
+                'label'=>"Date limite d'inscription",
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label'=>'Nombre de places',
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label'=>'Description et infos',
+            ])
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
